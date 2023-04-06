44cdd633c494de86bfeb5b203452682fd3fe42d5
DrakeSauce <almorin1@student.uiwtx.edu>
2023-04-06 16:19:46 -0500

    Added Movement

---
+++ Source/AM_Unreal_FinalProj/AThirdPersonCharBase.cpp
@@ -0,0 +1,215 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AThirdPersonCharBase.h"
+#include "AThirdPersonCharBase.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Animation/AnimInstanceProxy.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values
+AThirdPersonCharBase::AThirdPersonCharBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	attackRate = -1;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f,540.0f,0.0f);
+
+	cameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	cameraBoom->SetupAttachment(RootComponent);
+	cameraBoom->TargetArmLength = 300.0f;
+	cameraBoom->bUsePawnControlRotation = true;
+
+	followCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	followCamera->SetupAttachment(cameraBoom, USpringArmComponent::SocketName);
+	followCamera->bUsePawnControlRotation = false;
+
+	maxStamina = 100.0f;
+}
+
+// Called when the game starts or when spawned
+void AThirdPersonCharBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APlayerController* playerController = Cast<APlayerController>(GetController()))
+		if (UEnhancedInputLocalPlayerSubsystem* subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(playerController->GetLocalPlayer()))
+			subsystem->AddMappingContext(mappingContext, 0);
+
+	
+	maxStamina = 100;
+	stamina = maxStamina;
+	maxHealth = 100;
+	health = maxHealth;
+}
+
+// Called every frame
+void AThirdPersonCharBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	/*
+	if (bIsSprinting)
+	{
+		if (stamina > 1)
+			stamina -= 0.5f;
+		if (stamina < 1)
+			StopSprinting();
+	}
+	
+	if (!bIsSprinting && !bIsAttacking)
+	{
+		if (stamina < maxStamina)
+			stamina += 0.8f;
+	}
+
+	if (attackRate >= 0) { attackRate -= DeltaTime; }
+	else { bIsAttacking = false; }
+	*/
+}
+
+// Called to bind functionality to input
+void AThirdPersonCharBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* enhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		enhancedInputComponent->BindAction(moveAction, ETriggerEvent::Triggered, this, &AThirdPersonCharBase::MoveCharacter);
+		
+		enhancedInputComponent->BindAction(lookAction, ETriggerEvent::Triggered, this, &AThirdPersonCharBase::Look);
+
+		enhancedInputComponent->BindAction(attackAction, ETriggerEvent::Triggered, this, &AThirdPersonCharBase::LightAttack);
+		
+		enhancedInputComponent->BindAction(sprintAction, ETriggerEvent::Started, this, &AThirdPersonCharBase::Sprint);
+		enhancedInputComponent->BindAction(sprintAction, ETriggerEvent::Completed, this, &AThirdPersonCharBase::StopSprinting);
+		
+		//PlayerInputComponent->BindAxis("Turn", this, &AThirdPersonCharBase::AddControllerYawInput);
+		//PlayerInputComponent->BindAxis("LookUp", this, &AThirdPersonCharBase::AddControllerPitchInput);
+
+		//PlayerInputComponent->BindAction("Attack1",IE_Pressed,this,&AThirdPersonCharBase::LightAttack);
+		//PlayerInputComponent->BindAction("Sprint", IE_Pressed, this, &AThirdPersonCharBase::Sprint);
+		//PlayerInputComponent->BindAction("Sprint", IE_Released, this, &AThirdPersonCharBase::StopSprinting);
+	}
+}
+
+#pragma region Movement
+
+void AThirdPersonCharBase::MoveCharacter(const FInputActionValue& Value)
+{
+	const FVector2D moveVector = Value.Get<FVector2D>();
+
+	const FRotator rotation = Controller->GetControlRotation();
+	const FRotator yawRotation(0.0f, rotation.Yaw, 0.0f);
+
+	const FVector forwardDir = FRotationMatrix(yawRotation).GetUnitAxis(EAxis::X);
+	const FVector rightDir = FRotationMatrix(yawRotation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(forwardDir, moveVector.Y);
+	AddMovementInput(rightDir, moveVector.X);
+}
+
+void AThirdPersonCharBase::Look(const FInputActionValue& Value)
+{
+	const FVector2D lookAxisVector = Value.Get<FVector2D>();
+
+	AddControllerPitchInput(lookAxisVector.Y);
+	AddControllerYawInput(lookAxisVector.X);
+}
+
+#pragma endregion 
+
+#pragma region Sprint
+
+void AThirdPersonCharBase::Sprint()
+{
+	bIsSprinting = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+	
+	GetCharacterMovement()->MaxWalkSpeed *= sprintMultiplier;
+}
+
+void AThirdPersonCharBase::StopSprinting()
+{
+	bIsSprinting = false;
+
+	followCamera->bUsePawnControlRotation = false;
+	
+	GetCharacterMovement()->MaxWalkSpeed = 450.0f;
+}
+
+#pragma endregion 
+
+
+#pragma region Attack
+
+void AThirdPersonCharBase::LightAttack()
+{
+	//if (bIsAttacking) { return; }
+
+	//GetCharacterMovement()->DisableMovement();
+	stamina -= 25;
+
+	PlayAttackAnimEvent();
+	PlayAnimMontage(attackAnim1, 1, NAME_None);
+	
+	bIsAttacking = true;
+	attackRate = 1;
+	//bUseControllerRotationYaw = true;
+
+
+	/*
+	TArray<TEnumAsByte<EObjectTypeQuery>> objectTypes;
+	objectTypes.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_Pawn));
+	TArray<AActor*> actorsToIgnore;
+	actorsToIgnore.Add(this);
+	TArray<AActor*> outHit;
+
+	if (UKismetSystemLibrary::SphereOverlapActors(
+		GetController(),
+		GetActorLocation(),
+		200,
+		objectTypes,
+		nullptr,
+		actorsToIgnore,
+		outHit
+		))
+	{
+		for (AActor* hitObj : outHit)
+		{
+			if (IsValid(hitObj))
+			{
+				// Attack Whatever You're Attacking Here
+				
+				
+			}
+		}
+	}
+	*/
+}
+
+void AThirdPersonCharBase::StopLightAttack()
+{
+	GetCharacterMovement()->MovementMode = MOVE_Walking;
+
+	bIsAttacking = false;
+}
+
+
+
+
+
+
+
+
---
+++ Source/AM_Unreal_FinalProj/AThirdPersonCharBase.h
@@ -0,0 +1,95 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputAction.h"
+#include "Camera/CameraComponent.h"
+#include "Components/SphereComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "AThirdPersonCharBase.generated.h"
+
+UCLASS()
+class AM_UNREAL_FINALPROJ_API AThirdPersonCharBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AThirdPersonCharBase();
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+		UInputMappingContext* mappingContext;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* moveAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* lookAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* sprintAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* attackAction;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera")
+		USpringArmComponent* cameraBoom;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera")
+		UCameraComponent* followCamera;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera")
+		USphereComponent* sphereCol;
+	
+	UPROPERTY(EditAnywhere, Category="Movement")
+		float sprintMultiplier;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement")
+		float stamina;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement")
+		float maxStamina;
+	UPROPERTY(VisibleAnywhere, Category="Movement")
+		bool bIsSprinting;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation")
+		UAnimMontage* attackAnim1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation")
+		UAnimMontage* hitAnim;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Animation")
+		bool bIsAttacking = false;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Animation")
+		float attackRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement")
+		float health;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement")
+		float maxHealth;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Combat")
+		float damage;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UFUNCTION()
+		void MoveCharacter(const FInputActionValue& Value);
+	UFUNCTION()
+		void Look(const FInputActionValue& Value);
+	
+	void Sprint();
+	void StopSprinting();
+	
+	UFUNCTION(BlueprintImplementableEvent)
+		void PlayAttackAnimEvent();
+	UFUNCTION(BlueprintCallable)
+		void LightAttack();
+	UFUNCTION(BlueprintCallable)
+		void StopLightAttack();
+	
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
